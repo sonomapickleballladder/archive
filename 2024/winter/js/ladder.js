199db9fbb3fa14e649bdb96e3951c6553725de60
sonomapickleballladder <108966563+sonomapickleballladder@users.noreply.github.com>
2024-05-11 21:53:08 -0400

    Add files via upload

---
+++ 2024/winter/js/ladder.js
@@ -0,0 +1,5 @@
+const ladderSeason = '_Winter_2024';
+const thisLadder = 'Open';
+const laddersOffered = {'Open': {ls: '_08357319449750039424_', min: 0, sb: '', url: './'}, 'Womens': {ls: '_42543930859550627324_', min: 0, url: '@Womens/', sb: 'womens-ladder_'}}
+const ladderId = laddersOffered[thisLadder]
+const protoMap = {'http:': '.html', 'https:': '', 'file:': '.html'}
